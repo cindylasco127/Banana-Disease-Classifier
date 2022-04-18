d790e78ca235a9b42b2524274b349e4c5a851670
cindylasco127 <101797135+cindylasco127@users.noreply.github.com>
2022-04-18 13:29:00 +0800

    Update banana_disease.py

--- banana_disease.py
+++ banana_disease.py
@@ -35,7 +35,7 @@ st.title('Banana Disease Classifier')
 st.markdown("Prediction of the Banana Diseases: Bunchy Top, Moko, Sigatoka and Fusarium Wilt")
 
 def main():
-    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    file_uploaded = st.file_uploader("Select Image", type=["png","jpg","jpeg"])
     class_btn = st.button("Classify")
     if file_uploaded is not None:    
         image = Image.open(file_uploaded)
